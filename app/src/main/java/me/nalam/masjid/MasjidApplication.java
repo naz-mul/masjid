2fd67157f1f92f6790e63870cdae8fb58fd899d4
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2018-10-08 19:56:07 +0100

    Added Fabric Crashlytics and Answers Kit

--- app/src/main/java/me/nalam/masjid/MasjidApplication.java
+++ app/src/main/java/me/nalam/masjid/MasjidApplication.java
@@ -1,8 +1,17 @@
 package me.nalam.masjid;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import io.fabric.sdk.android.Fabric;
 
 public class MasjidApplication extends Application {
 
-
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    if (BuildConfig.USE_CRASHLYTICS) {
+      Fabric.with(this, new Crashlytics(), new Answers());
+    }
+  }
 }
